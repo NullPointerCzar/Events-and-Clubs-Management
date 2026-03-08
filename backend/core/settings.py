4b61335cc526e7edf96a1091c8b61ac8e664a02f
NullPointerCzar <sarwagyaformal@gmail.com>
2026-03-09 01:02:11 +0545

    host issue final fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-*xp6q6rypvr83&am#!h++rnl-l
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
