c30ae668c5df42909ec86cfa45aa71cb2381284e
NullPointerCzar <sarwagyaformal@gmail.com>
2026-03-09 00:12:13 +0545

    deployment issue fixed

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+cd backend
+
+# Install dependencies
 pip install -r requirements.txt
 
 # Convert static asset files
