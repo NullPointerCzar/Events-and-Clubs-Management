ba5219e180b2f0d71536c12f7913ae4171f1743b
NullPointerCzar <sarwagyaformal@gmail.com>
2026-03-09 15:34:00 +0545

    environment variable changing

--- build.sh
+++ build.sh
@@ -12,3 +12,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create admin user from environment variables if provided (non-fatal)
+python manage.py create_admin || true
