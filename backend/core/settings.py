0af68c8b9fc07171103c35e6dafdf715ce6c60e0
NullPointerCzar <sarwagyaformal@gmail.com>
2026-03-09 13:55:36 +0545

    cors errors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:3000,http://localhost:5173,http://127.0.0.1:5173,https://events-and-clubs-management.vercel.app'
+    'http://localhost:3000,http://localhost:5173,http://127.0.0.1:5173,https://events-and-clubs-management-54772uwg7.vercel.app'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
